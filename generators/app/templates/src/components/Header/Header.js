768f258885f954184e4ec98680edb743cdfe17b0
Ari Lerner <arilerner@mac.com>
2016-05-27 13:29:31 -0700

    Added adjustments to the generator and readme

--- generators/app/templates/src/components/Header/Header.js
+++ generators/app/templates/src/components/Header/Header.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { PropTypes as T } from 'react'
 import {Link} from 'react-router'
 
 import styles from './styles.module.css';
 
 export class Header extends React.Component {
   render() {
+    const {title} = this.props;
+
     return (
       <div className={styles.topbar}>
-        <Link className={styles.logo} to="/"><h1><%= title %></h1></Link>
+        <Link className={styles.logo} to="/"><h1>{title}</h1></Link>
         <section>
           Fullstack.io
         </section>
       </div>
     )
   }
+}
+
+Header.propTypes = {
+  title: T.string
+}
+
+Header.defaultProps = {
+  title: 'Change me'
 }
 
 export default Header
--- generators/app/templates/src/views/main/Container.js
+++ generators/app/templates/src/views/main/Container.js
@@ -8,9 +8,12 @@ export class Container extends React.Component {
   render() {
     return (
       <div className={styles.wrapper}>
-        <Header />
+        <Header tite="<%= title %>" />
         <div className={styles.content}>
-          <h1>Hello <%= title %></h1>
+          <h1>Hello from generated content</h1>
+          <p className={styles.padding}>
+            For more information about the `react-gen` package and set up, check out the <a href="https://www.fullstackreact.com/articles/react-tutorial-cloning-yelp/">Blog post on building a yelp clone with React</a>.
+          </p>
         </div>
       </div>
     )
