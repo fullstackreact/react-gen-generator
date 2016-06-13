264aa133c1f158e350cc6f08f4c192eff00a8389
Ari Lerner <arilerner@mac.com>
2016-06-13 15:30:56 -0700

    Fixed configureStore with the latest redux-module-builder

--- generators/app/templates/features/redux/redux/configureStore.js
+++ generators/app/templates/features/redux/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { browserHistory } from 'react-router';
-import { createApiMiddleware, bindActionCreatorsToStore } from 'redux-module-builder';
+import { bindActionCreatorsToStore } from 'redux-module-builder';
+import { createApiMiddleware } from 'redux-module-builder/api';
 import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, compose, applyMiddleware } from 'redux';
